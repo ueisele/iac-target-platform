ff3c1484caf7c8ee2e09f9a87e888b3f9094eddc
Uwe Eisele <uwe.eisele@online.de>
2017-11-25 14:51:50 +0100

    added traefik

--- destroy.sh
+++ destroy.sh
@@ -3,6 +3,7 @@ cd $(dirname $(readlink -f $0))
 
 sudo rm /etc/NetworkManager/dnsmasq.d/vagrant-landrush_platform-dev.conf || true
 sudo rm /etc/NetworkManager/dnsmasq.d/consul_platform-dev.conf || true
+sudo rm /etc/NetworkManager/dnsmasq.d/public_platform-dev.conf || true
 sudo systemctl restart NetworkManager
 
 vagrant destroy -f
--- up.sh
+++ up.sh
@@ -7,4 +7,5 @@ vagrant provision
 
 sudo sh -c 'echo "server=/platform.dev/127.0.0.1#10053" > /etc/NetworkManager/dnsmasq.d/vagrant-landrush_platform-dev.conf'
 sudo sh -c 'echo "server=/consul/192.168.17.20#8600" > /etc/NetworkManager/dnsmasq.d/consul_platform-dev.conf'
+sudo sh -c 'echo "address=/public.platform.dev/192.168.17.10" > /etc/NetworkManager/dnsmasq.d/public_platform-dev.conf'
 sudo systemctl restart NetworkManager
